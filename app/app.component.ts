4a58824798a8d83bf8016bc34870bdc8335985f6
rmmadhani <rmmadhani@gmail.com>
2022-12-13 21:48:28 +0530

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,26 +4,22 @@ import { Article } from './article/article.model'; // <-- import this
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular 5';
   articles: Article[]; // <-- component property
-  
+
   constructor() {
-  this.articles = [
-  new Article('Angular', 'http://angular.io', 3),
-  new Article('Fullstack', 'http://fullstack.io', 2),
-new Article('Angular Homepage', 'http://angular.io', 1),
-   ];
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
 
-
-
-
-  
-    addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    /* addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
      console.log(`Adding article title: ${title.value} and link: ${link.value}`);
      return false;
-     }
-    }
-
+     } */
+  }
+}
