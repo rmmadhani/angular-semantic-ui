991391f4d1ea5e1d2b547f18b4ab6b4fe5dba476
rmmadhani <rmmadhani@gmail.com>
2022-12-13 21:52:01 +0530

    1 file modified

--- app/article/article.component.ts
+++ app/article/article.component.ts
@@ -7,7 +7,9 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  
+  @Input() article: Article;
+
 
   constructor() {
     //this.article = new Article('Angular', 'http://angular.io', 10);
